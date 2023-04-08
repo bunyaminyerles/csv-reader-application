df17eeecbc1b73c9d7f974ed8dd93d5d470654dc
bunyaminyerles <bnymnyrls@hotmail.com>
2023-04-09 02:44:18 +0300

    it adds result table page

--- src/CsvImportPage.tsx
+++ src/CsvImportPage.tsx
@@ -5,28 +5,31 @@ function CsvImportPage() {
 
   const [loading, setLoading] = useState(false);
 
+  const onChange = (e: any) => {
+    setLoading(true);
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append('file', file);
+    // api call to upload csv file
+    axios.post('http://localhost:9000/csv/upload', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    }).then((response) => {
+      console.log(response);
+    }).catch((error) => {
+      console.log(error);
+    }).finally(() => {
+      setLoading(false);
+    });
+  };
+
   return (
     <div style={{display: 'flex', justifyContent: 'center'}}>
       {loading ?
         <div>Loading...</div>
         :
-        <input type="file" accept={'csv'} onChange={(e: any) => {
-          setLoading(true);
-          const file = e.target.files[0];
-          const formData = new FormData();
-          formData.append('file', file);
-          axios.post('http://localhost:9000/csv/upload', formData, {
-            headers: {
-              'Content-Type': 'multipart/form-data',
-            },
-          }).then((response) => {
-            console.log(response);
-          }).catch((error) => {
-            console.log(error);
-          }).finally(() => {
-            setLoading(false);
-          });
-        }}/>
+        <input type="file" accept={'csv'} onChange={(e: any) => onChange(e)}/>
       }
     </div>
   )
--- src/ResultTablePage.tsx
+++ src/ResultTablePage.tsx
@@ -1,8 +1,41 @@
+import {Button, Table} from "antd";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+
 function ResultTablePage() {
+
+  const [data, setData] = useState([]);
+  const [columns, setColumns] = useState<{ title: string, dataIndex: string }[]>([]);
+  const [loading, setLoading] = useState(false);
+  const getData = () => {
+    setLoading(true);
+    // api call to get data
+    axios.get('https://api.publicapis.org/entries').then((response) => {
+      if (Object.keys(response.data.entries[0]).length > 0) {
+        setData(response.data.entries)
+        var headers = Object.keys(response.data.entries[0]);
+        const tmpColumns = headers.map((col) => ({
+          title: col,
+          dataIndex: col
+        }));
+        setColumns(tmpColumns);
+      }
+    }).catch((error: any) => {
+      console.log(error);
+    }).finally(() => {
+      setLoading(false);
+    });
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
 
   return (
     <div>
-      <h1>Result Table</h1>
+      <Button onClick={() => getData()}> Re Fetch Data</Button>
+      <Table loading={loading} dataSource={data} columns={columns}/>
     </div>
   )
 }
